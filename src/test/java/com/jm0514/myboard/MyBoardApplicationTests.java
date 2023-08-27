50705b1ae713359b78df16926505443e86765523
JeongMin <skawjdals96@gmail.com>
2023-08-27 17:09:47 +0900

    First Commit

---
+++ src/test/java/com/jm0514/myboard/MyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm0514.myboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
