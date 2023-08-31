5058941eccb36dc7c27ddaa3c61d0b86f9fc363b
JeongMin <skawjdals96@gmail.com>
2023-08-31 22:55:14 +0900

    Feat: 로그인 Controller, Service 구현

--- src/main/java/com/jm0514/myboard/member/domain/Member.java
+++ src/main/java/com/jm0514/myboard/member/domain/Member.java
@@ -4,6 +4,10 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -15,15 +19,31 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    @Embedded
-    private Email email;
+    @Column(nullable = false, length = 30)
+    private String email;
 
-    @Embedded
-    private Username username;
+    @Column(nullable = false, length = 20)
+    private String name;
 
-    @Embedded
-    private Picture picture;
+    @Column(nullable = false)
+    private String profileImage;
 
     @Enumerated(EnumType.STRING)
     private RoleType roleType;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    public Member(final String email, final String name, final String profileImage) {
+        this.email = email;
+        this.name = name;
+        this.profileImage = profileImage;
+        this.roleType = RoleType.USER;
+        this.createdAt = LocalDateTime.now();
+        this.modifiedAt = LocalDateTime.now();
+    }
 }
