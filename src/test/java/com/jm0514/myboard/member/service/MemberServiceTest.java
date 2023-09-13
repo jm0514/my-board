4835f6775fe4574410ea483f855e008e6fa2a685
JeongMin <skawjdals96@gmail.com>
2023-09-13 16:41:35 +0900

    Test: 회원에 대한 Service에서 통합 테스트 추가

--- src/test/java/com/jm0514/myboard/member/service/MemberServiceTest.java
+++ src/test/java/com/jm0514/myboard/member/service/MemberServiceTest.java
@@ -3,7 +3,9 @@ package com.jm0514.myboard.member.service;
 import com.jm0514.myboard.auth.dto.AuthInfo;
 import com.jm0514.myboard.member.domain.Member;
 import com.jm0514.myboard.member.domain.RoleType;
+import com.jm0514.myboard.member.dto.MemberInfoRequestDto;
 import com.jm0514.myboard.member.dto.MemberInfoResponseDto;
+import com.jm0514.myboard.member.exception.NotFoundMemberException;
 import com.jm0514.myboard.member.repository.MemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -31,13 +33,7 @@ class MemberServiceTest {
     @Test
     void findMemberByAuthInfo(){
         // given
-        Member member = Member.builder()
-                .loginAccountId("1234")
-                .name("jeong-min")
-                .profileImageUrl("https://newsimg.sedaily.com/2023/07/19/29S6XZABI3_1.jpg")
-                .roleType(RoleType.USER)
-                .build();
-        Member savedMember = memberRepository.save(member);
+        Member savedMember = getSavedMember();
         Long memberId = savedMember.getId();
         AuthInfo authInfo = new AuthInfo(memberId, "USER");
 
@@ -47,5 +43,35 @@ class MemberServiceTest {
         // then
         assertThat(result).extracting("name", "profileImageUrl")
                 .contains("jeong-min", "https://newsimg.sedaily.com/2023/07/19/29S6XZABI3_1.jpg");
+    }
+
+    @DisplayName("로그인한 계정의 회원 정보를 변경할 수 있다.")
+    @Test
+    void updateMemberByAuthInfo(){
+        // given
+        Member savedMember = getSavedMember();
+        Long memberId = savedMember.getId();
+        AuthInfo authInfo = new AuthInfo(memberId, "USER");
+
+        MemberInfoRequestDto request = new MemberInfoRequestDto("min", "https://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg");
+
+        // when
+        memberService.updateMember(authInfo, request);
+        Member updatedMember = memberRepository.findById(memberId)
+                .orElseThrow(NotFoundMemberException::new);
+
+        // then
+        assertThat(updatedMember).extracting("name", "profileImageUrl")
+                .contains("min", "https://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg");
+    }
+
+    private Member getSavedMember() {
+        Member member = Member.builder()
+                .loginAccountId("1234")
+                .name("jeong-min")
+                .profileImageUrl("https://newsimg.sedaily.com/2023/07/19/29S6XZABI3_1.jpg")
+                .roleType(RoleType.USER)
+                .build();
+        return memberRepository.save(member);
     }
 }
