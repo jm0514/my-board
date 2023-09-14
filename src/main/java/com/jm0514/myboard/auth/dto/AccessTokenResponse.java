e5956a0ec71ab2eca99255f0aea47f9c8edccc9a
JeongMin <skawjdals96@gmail.com>
2023-09-14 21:57:18 +0900

    Refactor: 정적 팩토리 메서드로 수정

--- src/main/java/com/jm0514/myboard/auth/dto/AccessTokenResponse.java
+++ src/main/java/com/jm0514/myboard/auth/dto/AccessTokenResponse.java
@@ -6,9 +6,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AccessTokenResponse {
 
     private String accessToken;
+
+    public AccessTokenResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public static AccessTokenResponse from(String accessToken) {
+        return new AccessTokenResponse(accessToken);
+    }
 }
