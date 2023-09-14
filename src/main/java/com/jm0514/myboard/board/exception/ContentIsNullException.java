b7f22202fa1df17bbe0c43bcd5870fc47a8249fa
JeongMin <skawjdals96@gmail.com>
2023-09-14 21:54:58 +0900

    Feat: 게시판 작성에서 발생할 수 있는 예외 클래스 추가

---
+++ src/main/java/com/jm0514/myboard/board/exception/ContentIsNullException.java
@@ -0,0 +1,12 @@
+package com.jm0514.myboard.board.exception;
+
+import com.jm0514.myboard.advice.BadRequestException;
+
+public class ContentIsNullException extends BadRequestException {
+
+    private static final String MESSAGE = "글 내용은 1글자 이상 입력해야 합니다.";
+
+    public ContentIsNullException() {
+        super(MESSAGE);
+    }
+}
