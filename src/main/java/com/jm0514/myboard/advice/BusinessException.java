1bcd45e626b1d06decc9dee1eed53483a829b862
JeongMin <skawjdals96@gmail.com>
2023-10-05 23:48:43 +0900

    Test: 수정된 예외 처리 방식 적용(Enum)

--- src/main/java/com/jm0514/myboard/advice/BusinessException.java
+++ src/main/java/com/jm0514/myboard/advice/BusinessException.java
@@ -1,7 +1,9 @@
 package com.jm0514.myboard.advice;
 
 import com.jm0514.myboard.global.exception.ExceptionStatus;
+import lombok.Getter;
 
+@Getter
 public class BusinessException extends RuntimeException{
 
     private final String message;
