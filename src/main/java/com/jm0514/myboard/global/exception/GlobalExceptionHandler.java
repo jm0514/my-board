a2883424c3c6f0fab8c07f14ba884ead6257b23a
JeongMin <skawjdals96@gmail.com>
2023-10-07 22:02:52 +0900

    Feat: Logger 추가

--- src/main/java/com/jm0514/myboard/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jm0514/myboard/global/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.jm0514.myboard.global.exception;
 
-import com.jm0514.myboard.advice.AuthException;
-import com.jm0514.myboard.advice.BadRequestException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -14,29 +13,46 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.util.Objects;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            final MethodArgumentNotValidException ex,
+            final MethodArgumentNotValidException e,
             final HttpHeaders headers,
             final HttpStatusCode status,
             final WebRequest request
     ) {
-        String errorMessage = Objects.requireNonNull(ex.getBindingResult()
+        log.warn(e.getMessage(), e);
+
+        String errorMessage = Objects.requireNonNull(e.getBindingResult()
                 .getFieldError()).getDefaultMessage();
         return ResponseEntity.badRequest()
                 .body(new ExceptionResponse(errorMessage));
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handelException(final Exception e) {
+        log.error(e.getMessage(), e);
+
+        return ResponseEntity.internalServerError()
+                .body(new ExceptionResponse(e.getMessage()));
+    }
+
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ExceptionResponse> handleBadRequestException(final BadRequestException e) {
+    public ResponseEntity<ExceptionResponse> handleBadRequestException(
+            final BadRequestException e
+    ) {
+        log.warn(e.getMessage(), e);
+
         return ResponseEntity.badRequest()
                 .body(new ExceptionResponse(e.getMessage()));
     }
 
     @ExceptionHandler(AuthException.class)
     public ResponseEntity<ExceptionResponse> handleAuthException(final AuthException e) {
+        log.warn(e.getMessage(), e);
+
         return ResponseEntity.badRequest()
                 .body(new ExceptionResponse(e.getMessage()));
     }
