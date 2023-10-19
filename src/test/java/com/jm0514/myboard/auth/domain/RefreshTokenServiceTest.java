6c3704376b4f38c7d106d55da4c712902119f85d
JeongMin <skawjdals96@gmail.com>
2023-10-19 22:13:01 +0900

    Test: RefreshTokenService에 대한 통합 테스트 추가

---
+++ src/test/java/com/jm0514/myboard/auth/domain/RefreshTokenServiceTest.java
@@ -0,0 +1,78 @@
+package com.jm0514.myboard.auth.domain;
+
+import com.jm0514.myboard.auth.domain.repository.RefreshTokenRepository;
+import com.jm0514.myboard.global.IntegrationTestSupport;
+import com.jm0514.myboard.member.domain.Member;
+import com.jm0514.myboard.member.domain.RoleType;
+import com.jm0514.myboard.member.repository.MemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+class RefreshTokenServiceTest extends IntegrationTestSupport {
+
+    public static final String REFRESH_TOKEN = "refreshToken";
+
+    @Autowired
+    RefreshTokenRepository refreshTokenRepository;
+
+    @Autowired
+    RefreshTokenService refreshTokenService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @MockBean
+    JwtProvider jwtProvider;
+
+    @AfterEach
+    void tearDown() {
+        refreshTokenRepository.deleteAllInBatch();
+        memberRepository.deleteAllInBatch();
+    }
+
+    public static final String TOKEN = "token";
+    public static final long MEMBER_ID = 1L;
+
+    @DisplayName("리프레시 토큰을 저장할 수 있다.")
+    @Test
+    void savedToken() {
+        // given
+        Member createdMember = getMember();
+        memberRepository.save(createdMember);
+
+        // when then
+        refreshTokenService.saveToken(MEMBER_ID, TOKEN);
+    }
+
+    @DisplayName("저장된 리프레시 토큰과 요청된 리프레시 토큰이 같은지 비교한다.")
+    @Test
+    void match() {
+        // given
+        Member createdMember = getMember();
+        memberRepository.save(createdMember);
+
+        RefreshToken refreshToken = new RefreshToken(createdMember.getId(), REFRESH_TOKEN);
+        refreshTokenRepository.save(refreshToken);
+
+        given(jwtProvider.validateRefreshToken(any())).willReturn(true);
+
+        // when then
+        refreshTokenService.match(MEMBER_ID, REFRESH_TOKEN);
+
+    }
+
+    private Member getMember() {
+        return Member.builder()
+                .loginAccountId("1234")
+                .name("정민")
+                .profileImageUrl("Url")
+                .roleType(RoleType.USER)
+                .build();
+    }
+}
